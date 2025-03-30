2deccd27e2d6a17a2b88907d7101991e73d8d5d1
Mikhail Viaryha <mishutkavlesu@gmail.com>
2025-03-30 19:48:36 +0300

    feat: implement io interaction with predefined data

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
-package todoapp
+package main
+
+func main() {
+	newRecords := [][]string{
+		{"1", "Make a dinner", "2025-03-30T15:00:00.000Z"},
+		{"2", "Make the bed", "2025-03-30T15:20:05.045Z"},
+	}
+
+	file := createFile("records.csv")
+	writeCsvRecords(file, newRecords)
+
+	foundRecords := readCsvRecords(openFile("records.csv"))
+
+	printAligned(foundRecords)
+}
